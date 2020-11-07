ba4f320dd9db0d372d921b05a018f44cd62561ba
AnatoliiTymoshchuk <42546387+AnatoliiTymoshchuk@users.noreply.github.com>
2020-11-07 14:07:03 +0200

    Update ggl-prod.tf

--- ggl-prod.tf
+++ ggl-prod.tf
@@ -10,7 +10,7 @@ resource "google_compute_instance" "default" {
  machine_type = "f1-micro"
  boot_disk {
    initialize_params {
-     image = "ubuntu-1604-xenial-v20170328"
+     image = "Ubuntu16.04_64-bit"
    }
  }
  network_interface {
